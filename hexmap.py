a801a846d2c5d74976d8e7234c7dfedd13910877
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2018-01-06 16:54:34 +0100

    Change stroke-linecap to round to prevent intersection artifact at borders

--- hexmap.py
+++ hexmap.py
@@ -143,6 +143,7 @@ class HexmapEffect(inkex.Effect):
         line.set("y2", str(p2.y))
         line.set("stroke", "black")
         line.set("stroke-width", str(self.unittouu("%fin" % (1 / 72.0))))
+        line.set("stroke-linecap", "round")
         return line
 
     def svg_circle(self, p, radius):
@@ -162,6 +163,7 @@ class HexmapEffect(inkex.Effect):
         poly.set("points", pointsdef)
         poly.set("style", "stroke:none;fill:#ffffff;fill-opacity:1")
         poly.set("stroke-width", str(self.unittouu("%fin" % (1 / 72.0))))
+        poly.set("stroke-linecap", "round")
         return poly
 
     def svg_coord(self, p, col, row, cols, rows, anchor='middle'):
